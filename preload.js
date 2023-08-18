3488d139f7467b826ed99c0a69c84331a7f8e4c2
w1_liamby <weiliang_bai@foxmail.com>
2023-08-18 10:26:47 +0800

    node_modules 文件夹忽略

--- preload.js
+++ preload.js
@@ -1,5 +1,19 @@
 const electron = require('electron');   // electron 桌面集成，用于打开默认浏览器
-const axios = require('axios');
+const axios = require('axios'); // http 通讯
+const cheerio = require('cheerio'); // html 解析
+const { remark } = require('remark');
+const stripMk = require('strip-markdown');
+
+const topTabs = [];   // OI-Wiki 顶部导航标签
+
+class UtoolsListItem {
+    constructor(title, description, url, icon) {
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.icon = icon;
+    }
+}
 
 function stripHTMLAndEscapes(html) {
     let res = html.replace(/<.*?>/g, '');
@@ -8,18 +22,23 @@ function stripHTMLAndEscapes(html) {
     return res;
 }
 
-/* 去掉 Markdown 标题的 # 号 */
-function removeMarkdownTitleSharp(title) {
-    return title.replace(/^\s*#*/g, '').trim();
+/* 去掉 Markdown 格式 */
+function stripMarkdownFormat(mk) {
+    let res = null;
+    remark().use(stripMk).process(mk)
+        .then((text) => {
+            res = text;
+        });
+    return res;
 }
 
 function processItem(item) {
     let newItem = new Object();
-    newItem.title = removeMarkdownTitleSharp(stripHTMLAndEscapes(item.title));
+    newItem.title = stripMarkdownFormat(stripHTMLAndEscapes(item.title));
     if (newItem.title.length === 0) {
         newItem.title = '------';
     }
-    newItem.highlight = stripHTMLAndEscapes(`${(item.highlight || '无简介').concat()}`);
+    newItem.highlight = stripMarkdownFormat(stripHTMLAndEscapes(`${(item.highlight || '无简介').concat()}`));
     newItem.url = item.url;
     return newItem;
 }
@@ -31,7 +50,24 @@ window.exports = {
             // 进入插件应用时调用（可选）
             enter: (action, callbackSetList) => {
                 // 如果进入插件应用就要显示列表数据
-                
+                    axios.get('https://oi-wiki.org/').then(
+                        (response) => {
+                            let $mainPage = cheerio.load(response.data);
+                            $mainPage('a', 'li[class=md-tabs__item]').each(
+                                (i, elem) => {
+                                    topTabs.push(new UtoolsListItem(
+                                        $(elem).text().trim(),
+                                        '简介',
+                                        $(elem).attr('href'),
+                                        'img/title',
+                                    ));
+                                }
+                            );
+                            callbackSetList(topTabs);
+                        }
+                    ).catch(
+                        (error) => {callbackSetList(['error']);}
+                    )
             },
             // 子输入框内容变化时被调用 可选 (未设置则无搜索)
             search: (action, searchWord, callbackSetList) => {
@@ -52,12 +88,12 @@ window.exports = {
                             let presentList = [];
                             matchedList.forEach(item => {
                                 let newItem = processItem(item)
-                                presentList.push({
-                                    title: newItem.title,
-                                    description: newItem.highlight,
-                                    url: `https://oi-wiki.org${newItem.url}`,
-                                    icon: 'img/search.png'
-                                });
+                                presentList.push(new UtoolsListItem(
+                                    newItem.title,
+                                    newItem.highlight,
+                                    `https://oi-wiki.org${newItem.url}`,
+                                    'img/search.png'
+                                ));
                             });
                             // 执行 callbackSetList 显示出来
                             callbackSetList(presentList);
